655d4e9334890a8ca589b8104d2415477461edaa
BethGomez44 <liz_gomez04@ciencias.unam.mx>
2020-10-04 17:12:58 -0500

    Ordenando

---
+++ Laboratorio/Clase_1.c
@@ -0,0 +1,29 @@
+//Clase 1 - Introducción a C
+
+#include <stdio.h>
+
+int main(){
+    printf("Hola, mundo\n");
+    int a = 3;
+    float b = 4.5;
+    double c = 5.25;
+    float suma;
+
+    suma = a + b + c;
+
+    printf("La suma de a,b,c es %f.", suma);
+    return 0;
+}
+
+/** Boleanos
+ *#define BOOL char
+ *#define FALSE 0
+ *#define TRUE 1*/
+
+/**int a = 0;
+int a = 0, b = 1, c = 2;*/
+
+/**
+ * Definir matrices
+ * int numbers[10];
+*/
---
+++ Proyectos/Web Service Clima/run.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+java -cp .:./build:./build/almacen/.*:./build/estructuras/.*:./build/menus/.*.:./build/funcionalidades/.* Main
